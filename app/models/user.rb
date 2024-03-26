b14b96a09f37f7aa052acef3abb32181e51b0401
MrBabon <christophedanna06@gmail.com>
2024-03-26 17:57:19 +0100

    edit fini + retouche profil

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,43 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+  validates :first_name, presence: true, length: { maximum: 17 }, format: { without: /\s/ }
+  validates :last_name, presence: true, length: { maximum: 20 }, format: { without: /\s/ }
+  validates :phone, presence: true, length: { maximum: 20 }
+  validates :biography, length: { maximum: 1000 }
 
+  enum industry: {
+    agriculture: "Agriculture",
+    art_design: "Art & Design",
+    education: "Education",
+    energy: "Energy",
+    engineering: "Engineering",
+    environment: "Environment",
+    finance: "Finance",
+    health: "Healthcare",
+    human_resources: "Human Resources",
+    manufacturing: "Manufacturing",
+    media: "Media",
+    professional_services: "Professional Services",
+    public_administration: "Public Administration",
+    real_estate: "Real Estate",
+    retail: "Retail",
+    science: "Science",
+    technology: "Technology",
+    telecommunications: "Telecommunications",
+    tourism: "Tourism",
+    transportation: "Transportation",
+  }
 
+  validates :industry, inclusion: { in: industries.keys, message: "Industry invalid" }, allow_blank: true
+
+ 
   def full_name
     "#{first_name.capitalize} #{last_name.capitalize}"
+  end
+
+  def industry_form_value
+    industry.presence || "Industry not specified"
   end
   
 end
